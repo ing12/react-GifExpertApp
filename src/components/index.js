0e996436b75b44a8633de27be40db19a9be94953
ing12 <mtra.leslie.isabel@gmail.com>
2024-05-06 16:02:49 -0600

    hvkh

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@
 export * from './AddCategory'
 export * from './GifGrid'
 export * from './GifItem'
+
+//cuando se importe este fichero solo haremos referencia a la carpeta y no al fichero pues al nombrarlo index autmaticamente lo toma.
